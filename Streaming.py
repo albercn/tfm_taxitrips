1a40e9a3789d0b1f4843f186d751386814dd8b43
alberto_cuesta <cn.alberto@gmail.com>
2017-10-14 18:17:46 +0200

    Estracción de información de kafka y escritura al topic de salida

---
+++ Streaming.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# Este archivo usa el encoding: utf-8
+
+from __future__ import print_function
+
+import sys
+import json
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+from kafka import KafkaProducer
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: Streaming.py.py <broker_list> <topic>", file=sys.stderr)
+        exit(-1)
+
+
+# Función para enviar el contenidod de cada partición al topic de salida de kafka
+def sendPartition(particion):
+
+    productor = KafkaProducer(bootstrap_servers=brokers)
+
+    for r in particion:
+        productor.send('outTopic', str(r[1]))
+
+    productor.close()
+
+# Create a local StreamingContext with two working thread and batch interval of 1 second
+sc = SparkContext("local[2]", "StreamingTaxis")
+ssc = StreamingContext(sc, 1)
+
+# Creación del Input Direct DStream de kafka leyendo del receiver de kafka
+brokers, topic = sys.argv[1:]
+kst = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
+
+kst.count().pprint()
+
+kst.foreachRDD(lambda rdd: rdd.foreachPartition(sendPartition))
+
+ssc.start()
+ssc.awaitTermination()
