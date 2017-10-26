718f0d3380000e84e6c614890c876b1498ca7d8c
alberto_cuesta <cn.alberto@gmail.com>
2017-10-26 18:38:39 +0200

    Renombrado

---
+++ kafkaTaxiTripsProducer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# Este archivo usa el encoding: utf-8
+
+import os
+import time, datetime
+import json
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+fileName = os.path.join('data_source', 'Taxi_Trips_2017.json')
+
+infile = open(fileName, 'r')
+for line in infile:
+    lineJson = json.loads(line)
+
+    duration = datetime.timedelta(seconds=int(lineJson['Trip_Seconds']))
+    t = datetime.datetime.now()
+    end = t + duration
+
+    lineJson['Trip_Start_Timestamp'] = t.strftime("%m/%d/%Y %I:%M:%S %p")
+    lineJson['Trip_End_Timestamp'] = end.strftime("%m/%d/%Y %I:%M:%S %p")
+
+    strLineJson = json.dumps(lineJson)
+
+    producer.send(topic="rawTopic", value=strLineJson)
+
+    time.sleep(0.5)
+
+infile.close()
